8ca674f59a86f910ec5047154268c0cb73a57100
Jason Stephens <jassteph@redhat.com>
2022-04-06 15:49:44 -0400

    adding createShimService to refactored frontends

--- controllers/cloud.redhat.com/frontends.go
+++ controllers/cloud.redhat.com/frontends.go
@@ -2,11 +2,13 @@ package controllers
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"strings"
 
 	clowder "github.com/RedHatInsights/clowder/apis/cloud.redhat.com/v1alpha1"
 	frontend "github.com/RedHatInsights/frontend-operator/api/v1alpha1"
+	core "k8s.io/api/core/v1"
 	k8serr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -58,9 +60,59 @@ func CreateFrontendEnv(ctx context.Context, cl client.Client, nsName string, clo
 			},
 		})
 
-		err = cl.Create(ctx, &frontendEnv)
-		return err
+		if err := cl.Create(ctx, &frontendEnv); err != nil {
+			return err
+		}
+
+		// create "shim" services for keycloak, mbop, mocktitlements
+		// this is a temporary solution until apps begin to read the hostnames for these from their cdappconfig.json
+		if err := createShimServices(ctx, cl, ns, clowdEnv); err != nil {
+			return err
+		}
+
+		return nil
 	}
 
 	return err
+}
+
+func createShimServices(ctx context.Context, cl client.Client, ns core.Namespace, clowdEnv clowder.ClowdEnvironment) error {
+	serviceNames := []string{"keycloak", "mbop", "mocktitlements"}
+
+	for _, serviceName := range serviceNames {
+		origSvc := core.Service{}
+		nn := types.NamespacedName{
+			Name:      fmt.Sprintf("%s-%s", clowdEnv.Name, serviceName),
+			Namespace: ns.Name,
+		}
+
+		err := cl.Get(ctx, nn, &origSvc)
+		if err != nil {
+			continue
+		}
+
+		// create a new Service with the same configuration spec but a non-prefixed name
+		newSvc := core.Service{}
+		newSvc.SetName(serviceName)
+		newSvc.SetNamespace(ns.Name)
+		newSvc.Spec = origSvc.Spec
+		// empty the ClusterIPs so a new one is generated
+		newSvc.Spec.ClusterIP = ""
+		newSvc.Spec.ClusterIPs = []string{}
+		newSvc.SetOwnerReferences([]metav1.OwnerReference{
+			{
+				APIVersion: ns.APIVersion,
+				Kind:       ns.Kind,
+				Name:       ns.Name,
+				UID:        ns.UID,
+			},
+		})
+
+		err = cl.Create(ctx, &newSvc)
+		if err != nil {
+			return errors.New(fmt.Sprintf("failed to create shim service for '%s'", serviceName))
+		}
+	}
+
+	return nil
 }
