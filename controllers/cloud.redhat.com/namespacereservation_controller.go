7e773ab19c8c0ad79badc0b81237c4583eb62f75
Matt Knop <mknop@redhat.com>
2022-06-22 11:55:48 -0600

    Added check if err is not nil.

--- controllers/cloud.redhat.com/namespacereservation_controller.go
+++ controllers/cloud.redhat.com/namespacereservation_controller.go
@@ -96,7 +96,10 @@ func (r *NamespaceReservationReconciler) Reconcile(ctx context.Context, req ctrl
 			return ctrl.Result{}, err
 		}
 		if r.Poller.namespaceIsExpired(expirationTS) {
-			DeletePrometheusOperator(ctx, r.Client, res.Status.Namespace)
+			err := DeletePrometheusOperator(ctx, r.Client, res.Status.Namespace)
+			if err != nil {
+				r.Log.Error(err, fmt.Sprintf("Error deleting prometheus.%s", res.Status.Namespace))
+			}
 
 			if err := r.Client.Delete(ctx, &res); err != nil {
 				r.Log.Error(err, "Unable to delete waiting reservation", "res-name", res.Name)
