9817e37c5aaedf1c3d69ae45b0f591253935203b
Matt Knop <mknop@redhat.com>
2022-06-27 13:43:37 -0600

    Returned the result from controller runtime.

--- controllers/cloud.redhat.com/pool_controller.go
+++ controllers/cloud.redhat.com/pool_controller.go
@@ -59,6 +59,7 @@ func (r *NamespacePoolReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 	err = r.handleErrorNamespaces(ctx, errNamespaceList)
 	if err != nil {
 		r.Log.Error(err, "Unable to delete object.")
+		return ctrl.Result{}, err
 	}
 
 	r.Log.Info("Pool status", "ready", status["ready"], "creating", status["creating"])
