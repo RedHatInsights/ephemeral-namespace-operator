4c60a633daf672760d2db0ae8f8fafef11ad4a90
Matt Knop <mknop@redhat.com>
2023-01-09 10:54:10 -0700

    [RHCLOUD-23387] Updated go version and dependencies (#151)
    * Updated go version
    * Updates to Ginkgo tests
    * Updated dependencies, code adjustments.
    * Updated github actions golang version
    * Changed Actions file name, removed comments
    * Create version.txt

--- controllers/cloud.redhat.com/suite_test.go
+++ controllers/cloud.redhat.com/suite_test.go
@@ -22,7 +22,7 @@ import (
 	"testing"
 	"time"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -31,7 +31,6 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
-	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
@@ -54,9 +53,9 @@ var stopController context.CancelFunc
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
 
-	RunSpecsWithDefaultAndCustomReporters(t,
+	RunSpecs(t,
 		"Controller Suite",
-		[]Reporter{printer.NewlineReporter{}})
+		Label("Ephemeral Namespace Operator"))
 }
 
 // routine that will auto-update ClowdEnvironment status during suite test run
@@ -270,7 +269,7 @@ var _ = BeforeSuite(func() {
 		Expect(err).ToNot(HaveOccurred())
 	}()
 
-}, 60)
+})
 
 var _ = AfterSuite(func() {
 	By("tearing down the test environment")
