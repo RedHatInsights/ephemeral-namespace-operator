59e235fa77daf7fb30cc90859f004d46bea40edc
Matt Knop <mknop@redhat.com>
2023-01-17 12:16:08 -0700

    Fix linter issue

--- controllers/cloud.redhat.com/namespace_controller_suite_test.go
+++ controllers/cloud.redhat.com/namespace_controller_suite_test.go
@@ -4,16 +4,18 @@ import (
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("", func() {
 	const (
 		timeout  = time.Second * 35
-		duration = time.Second * 30
 		interval = time.Millisecond * 30
 	)
 
 	Context("", func() {
-
+		Eventually(func() bool {
+			return true
+		}, timeout, interval).Should(BeTrue())
 	})
 })
