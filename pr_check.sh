eb262d57485425e8b2cbd214219888ab7efac01d
Matt Knop <mknop@redhat.com>
2023-01-25 18:07:33 -0700

    Adding env back.

--- pr_check.sh
+++ pr_check.sh
@@ -47,6 +47,7 @@ TEST_CONT="ephemeral-namespace-operator-unit-"$IMAGE_TAG
 docker build -t $TEST_CONT -f Dockerfile.test --build-arg BASE_IMAGE=$BASE_IMG . 
 
 docker run -i \
+    -v `$PWD/testbin use -p path`:/bins:ro \
     -e IMAGE_NAME=$IMAGE_NAME \
     -e IMAGE_TAG=$IMAGE_TAG \
     -e QUAY_USER=$QUAY_USER \
@@ -79,6 +80,8 @@ docker rm -f $CONTAINER_NAME || true
 set +e
 docker run -i \
     --name $CONTAINER_NAME \
+    -v $PWD:/workspace:ro \
+    -v `$PWD/bin/setup-envtest use -p path`:/bins:ro \
     -e IMAGE_NAME=$IMAGE_NAME \
     -e IMAGE_TAG=$IMAGE_TAG \
     -e QUAY_USER=$QUAY_USER \
