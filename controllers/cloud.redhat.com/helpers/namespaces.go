2a7aecccb96c104d100b380e32b897ccddc6b182
Matt Knop <mknop@redhat.com>
2023-01-31 10:22:49 -0700

    [WIP] Rogue namespaces bug fix, Namespace annotations aren't updating (#160)
    * Get resource before updating resource
    
    * Fixed error message
    
    * Removed get resource logic
    
    * Retry update on error
    
    * Retry on conflict.
    
    * Remove wait on backoff

--- controllers/cloud.redhat.com/helpers/namespaces.go
+++ controllers/cloud.redhat.com/helpers/namespaces.go
@@ -135,17 +135,24 @@ func CheckReadyStatus(pool string, ns core.Namespace, ready []core.Namespace) []
 	return ready
 }
 
-func UpdateAnnotations(ctx context.Context, cl client.Client, nsName string, annotations map[string]string) error {
-	ns, err := GetNamespace(ctx, cl, nsName)
+func UpdateAnnotations(ctx context.Context, cl client.Client, namespaceName string, annotations map[string]string) error {
+	namespace, err := GetNamespace(ctx, cl, namespaceName)
 	if err != nil {
 		return err
 	}
 
-	utils.UpdateAnnotations(&ns, annotations)
+	utils.UpdateAnnotations(&namespace, annotations)
 
-	if err := cl.Update(ctx, &ns); err != nil {
-		return fmt.Errorf("there was issue updating annotations for namespace [%s]", ns.Name)
-	}
+	err = retry.RetryOnConflict(
+		retry.DefaultBackoff,
+		func() error {
+			if err = cl.Update(ctx, &namespace); err != nil {
+				return fmt.Errorf("there was an issue updating annotations for namespace [%s]", namespaceName)
+			}
+
+			return nil
+		},
+	)
 
 	return nil
 }
