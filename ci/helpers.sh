6a04b830c94c40b7f2e875e44a1c14413c2a69ca
Matt Knop <mknop@redhat.com>
2023-08-07 15:07:03 -0600

    Same docker go-toolset image

--- ci/helpers.sh
+++ ci/helpers.sh
@@ -5,7 +5,7 @@ set -x
 CONTAINER_ENGINE_CMD=''
 TEST_CONTAINER_NAME=''
 TEARDOWN_RAN=0
-GO_TOOLSET_IMAGE='registry.access.redhat.com/ubi8/go-toolset:1.19.9-2.1687187497'
+GO_TOOLSET_IMAGE='registry.access.redhat.com/ubi8/go-toolset:1.19.10-3'
 
 get_N_chars_commit_hash() {
 
